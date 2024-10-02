726fe0f445c8b436d14547ce66a3e194c37a62eb
NaeNae4321 <debruler.nl@gmail.com>
2024-10-02 18:19:38 -0400

    I will not make a merge conflictgit add .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode;
 public class Test {
     //Testing... Testing...
     //Person 1 test here
-    //Can this create a merge conflict?
+    //Can this create a merge conflict? No it can't.
 }
