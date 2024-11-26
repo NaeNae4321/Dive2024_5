612c973b2fa2e8bdfab4b425d089fdd85f036b94
NaeNae4321 <debruler.nl@gmail.com>
2024-11-26 10:41:20 -0500

    Fixed offsets and finished test auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/PinpointLocalizer.java
@@ -70,7 +70,7 @@ public class PinpointLocalizer extends Localizer {
         odo = hardwareMap.get(GoBildaPinpointDriver.class,"computer");
 
         //This uses mm, to use inches divide these numbers by 25.4
-        odo.setOffsets(-60.0, -12.0); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setOffsets(-12.0, -60.0); //these are tuned for 3110-0002-0001 Product Insight #1
         //TODO: If you find that the gobilda Yaw Scaling is incorrect you can edit this here
       //  odo.setYawScalar(1.0);
         //TODO: Set your encoder resolution here, I have the Gobilda Odometry products already included.
